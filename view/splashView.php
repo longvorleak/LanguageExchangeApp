d5e54671e52f9929e1d211e814044c8f5863019b
Sam <samkim0822@gmail.com>
2022-09-13 17:15:26 +0900

    finished off language slide down menu

--- view/splashView.php
+++ view/splashView.php
@@ -12,17 +12,3 @@
 
 <?php require("template.php")?>
 
-
-<?php $title = "splash" ?>
-
-<?php ob_start();?>
-
-<?php include("headerView.php"); ?>
-<?php include("sectionsView.php"); ?>
-<?php include("footerView.php"); ?>
-
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require("template.php")?>
-
