bfb0822360824a38a3c13cd6ef353294b38869ac
Sam <samkim0822@gmail.com>
2022-09-16 14:49:27 +0900

    align items in dashboard

--- view/home.php
+++ view/home.php
@@ -3,29 +3,7 @@
 <?php $title = "Dashboard" ?>
 
 <?php ob_start();?>
-    <section class="dashboard">
-        <h2>Menu</h2>
-        <nav role='navigation'>
-            <div id="menuToggle">
-
-                <input id="hamburger-checkbox" type="checkbox" />
-                
-                <span></span>
-                <span></span>
-                <span></span>
-                <ul id="menu">
-                    <a href="#"><li><img src="https://cdn-icons-png.flaticon.com/512/149/149071.png"/>My Account</li></a>
-                    <a href="#"><li><i class="fa-solid fa-house home-icon"></i>Home</li></a>
-                    <a href="#"><li><i class="fa-solid fa-user-group home-icon"></i>Browse</li></a>
-                    <a href="#"><li><i class="fa-solid fa-graduation-cap home-icon"></i>Learn</li></a>
-                    <a href="#"><li><i class="fa-solid fa-calendar home-icon"></i>Events</li></a>
-                    <a href="#"><li><i class="fa-solid fa-gear home-icon"></i>Settings</li></a>
-                    <a href="#"><li>Upgrade</li></a>
-                    <a href="#"><li>Sign out</li></a>
-                </ul>
-            </div>
-        </nav>
-    </section>
+<?php include("side_menuView.php") ?>
     <section class="middle-section">
 
         <div class="middle1">
--- view/profileView.php
+++ view/profileView.php
@@ -1,1 +1,42 @@
-<!-- comment -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="profile-settings.css"/>
+    <title>Document</title>
+</head>
+<body>
+    <?php include("side_menuView.php") ?>
+    <section>
+        <h1>Profile</h1> <br>
+        <form>
+            <span> 
+                <img src=""/> 
+                <input type="button" value="Change"/> 
+                <input type="button" value="remove"/>
+            </span>
+            <span>
+                <label for="first-name">First name </label>
+                <label for="last-name">Last name </label> 
+                <br>
+                <input type=text id="first-name" value=""/> 
+                <input type=text id="last-name" value=""/> 
+            </span>
+            <span>
+                <label for="username">Username</label>
+                <br>    
+                <input type="text" value=""></label>
+            </span>
+            <span>
+                <label for="bio">Your bio</label>
+                <br>
+                <input type="textbox" value=""/>
+            </span>
+
+                
+    </section>
+    
+</body>
+</html>
