bdcf4aac0e32394bb61653b9e34ed7c2b2df91e8
sude <sumeyyailkin@gmail.com>
2022-09-20 16:59:41 +0900

    language list added to the db

--- model/language_exchange_app.sql
+++ model/language_exchange_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2022 at 04:10 PM
+-- Generation Time: Sep 20, 2022 at 04:55 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -99,16 +99,122 @@ INSERT INTO `known_language` (`id`, `user_id`, `language_id`, `proficiency`) VAL
 
 CREATE TABLE `language` (
   `id` int(11) NOT NULL,
-  `language` varchar(255) NOT NULL
+  `language` varchar(255) NOT NULL,
+  `abbrevetion` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `language`
 --
 
-INSERT INTO `language` (`id`, `language`) VALUES
-(1, 'English'),
-(2, 'Korean');
+INSERT INTO `language` (`id`, `language`, `abbrevetion`) VALUES
+(1, 'English', 'en'),
+(2, 'German', 'de'),
+(3, 'French', 'fr'),
+(4, 'Dutch', 'nl'),
+(5, 'Italian', 'it'),
+(6, 'Spanish', 'es'),
+(7, 'Polish', 'pl'),
+(8, 'Russian', 'ru'),
+(9, 'Japanese', 'ja'),
+(10, 'Portuguese', 'pt'),
+(11, 'Swedish', 'sv'),
+(12, 'Chinese', 'zh'),
+(13, 'Catalan', 'ca'),
+(14, 'Ukrainian', 'uk'),
+(15, 'Norwegian', 'no'),
+(16, 'Finnish', 'fi'),
+(17, 'Vietnamese', 'vi'),
+(18, 'Czech', 'cs'),
+(19, 'Hungarian', 'hu'),
+(20, 'Korean', 'ko'),
+(21, 'Indonesian', 'id'),
+(22, 'Turkish', 'tr'),
+(23, 'Romanian', 'ro'),
+(24, 'Persian', 'fa'),
+(25, 'Arabic', 'ar'),
+(26, 'Danish', 'da'),
+(27, 'Esperanto', 'eo'),
+(28, 'Serbian', 'sr'),
+(29, 'Lithuanian', 'lt'),
+(30, 'Slovak', 'sk'),
+(31, 'Malay', 'ms'),
+(32, 'Hebrew', 'he'),
+(33, 'Bulgarian', 'bg'),
+(34, 'Slovenian', 'sl'),
+(35, 'VolapÃ¼k', 'vo'),
+(36, 'Kazakh', 'kk'),
+(37, 'Waray-Waray', 'war'),
+(38, 'Basque', 'eu'),
+(39, 'Croatian', 'hr'),
+(40, 'Hindi', 'hi'),
+(41, 'Estonian', 'et'),
+(42, 'Azerbaijani', 'az'),
+(43, 'Galician', 'gl'),
+(44, 'Simple English', 'simple'),
+(45, 'Norwegian (Nynorsk)', 'nn'),
+(46, 'Thai', 'th'),
+(47, 'Newar / Nepal Bhasa', 'new'),
+(48, 'Greek', 'el'),
+(49, 'Aromanian', 'roa-rup'),
+(50, 'Latin', 'la'),
+(51, 'Occitan', 'oc'),
+(52, 'Tagalog', 'tl'),
+(53, 'Haitian', 'ht'),
+(54, 'Macedonian', 'mk'),
+(55, 'Georgian', 'ka'),
+(56, 'Serbo-Croatian', 'sh'),
+(57, 'Telugu', 'te'),
+(58, 'Piedmontese', 'pms'),
+(59, 'Cebuano', 'ceb'),
+(60, 'Tamil', 'ta'),
+(61, 'Belarusian (TaraÅ¡kievica)', 'be-x-old'),
+(62, 'Breton', 'br'),
+(63, 'Latvian', 'lv'),
+(64, 'Javanese', 'jv'),
+(65, 'Albanian', 'sq'),
+(66, 'Belarusian', 'be'),
+(67, 'Marathi', 'mr'),
+(68, 'Welsh', 'cy'),
+(69, 'Luxembourgish', 'lb'),
+(70, 'Icelandic', 'is'),
+(71, 'Bosnian', 'bs'),
+(72, 'Yoruba', 'yo'),
+(73, 'Malagasy', 'mg'),
+(74, 'Aragonese', 'an'),
+(75, 'Bishnupriya Manipuri', 'bpy'),
+(76, 'Lombard', 'lmo'),
+(77, 'West Frisian', 'fy'),
+(78, 'Bengali', 'bn'),
+(79, 'Ido', 'io'),
+(80, 'Swahili', 'sw'),
+(81, 'Gujarati', 'gu'),
+(82, 'Malayalam', 'ml'),
+(83, 'Western Panjabi', 'pnb'),
+(84, 'Afrikaans', 'af'),
+(85, 'Low Saxon', 'nds'),
+(86, 'Sicilian', 'scn'),
+(87, 'Urdu', 'ur'),
+(88, 'Kurdish', 'ku'),
+(89, 'Cantonese', 'zh-yue'),
+(90, 'Armenian', 'hy'),
+(91, 'Quechua', 'qu'),
+(92, 'Sundanese', 'su'),
+(93, 'Nepali', 'ne'),
+(94, 'Zazaki', 'diq'),
+(95, 'Asturian', 'ast'),
+(96, 'Tatar', 'tt'),
+(97, 'Neapolitan', 'nap'),
+(98, 'Irish', 'ga'),
+(99, 'Chuvash', 'cv'),
+(100, 'Samogitian', 'bat-smg'),
+(101, 'Walloon', 'wa'),
+(102, 'Amharic', 'am'),
+(103, 'Kannada', 'kn'),
+(104, 'Alemannic', 'als'),
+(105, 'Buginese', 'bug'),
+(106, 'Burmese', 'my'),
+(107, 'Interlingua', 'ia');
 
 -- --------------------------------------------------------
 
@@ -155,7 +261,11 @@ CREATE TABLE `target_language` (
 --
 
 INSERT INTO `target_language` (`id`, `user_id`, `language_id`, `proficiency`) VALUES
-(1, 1, 2, 1);
+(1, 1, 2, 1),
+(2, 1, 3, 3),
+(3, 1, 4, 4),
+(4, 2, 1, 2),
+(5, 2, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -191,7 +301,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `uid`, `firstname`, `lastname`, `username`, `email`, `password`, `dob`, `date_created`, `last_login`, `gender`, `city_id`, `premium_id`, `interests`, `hometown`, `profilepic`, `introduction`, `is_active`) VALUES
 (1, 'F2ShTDW5wS', 'd', 'd', 'ds', 's@ss.com', '$2y$10$.Zk92NMkYEqSCBTR94uq5.T48bQFt/usJu9.LjG6iwsbYb8NgrXQG', '0000-00-00', '2022-09-14 16:30:05', '2022-09-14 16:30:05', 0, 0, NULL, '', 0, '', '', 1),
 (2, 'xZMhFUKyg9', 'second', 'a', 'aa', 'a@aa.com', '$2y$10$cP7jm3ZhD2i8hgq3SePOpu5WkMyC4WDxSSQwZWfT6aIpvwWkT91P2', '0000-00-00', '2022-09-14 16:51:22', '2022-09-14 16:51:22', 0, 0, NULL, '', 0, '', '', 1),
-(3, 'rKnQCF1uzT', 'thirr', 's', 'oo', 'oo@oo.com', '$2y$10$AMfTFImAvZ/olRQMDsl8gu4rHqpZcSejKKCQHOTH0dBEAnPpuP8M2', '0000-00-00', '2022-09-14 17:02:37', '2022-09-14 17:02:37', 0, 0, NULL, '', 0, '', '', 1);
+(3, 'rKnQCF1uzT', 'thirr', 's', 'oo', 'oo@oo.com', '$2y$10$AMfTFImAvZ/olRQMDsl8gu4rHqpZcSejKKCQHOTH0dBEAnPpuP8M2', '0000-00-00', '2022-09-14 17:02:37', '2022-09-14 17:02:37', 0, 0, NULL, '', 0, '', '', 1),
+(4, 'uMtMW9TprQ', '', '', 'four', 'four@four.com', '$2y$10$iGP7gLyVuYKzmWFGQV3jNuIpAGQzccZYsrE6cx/bPCcy8NFQl64rW', '2022-09-20', '2022-09-20 16:15:16', '2022-09-20 16:15:16', 0, 0, NULL, '', 0, '', '', 1);
 
 --
 -- Indexes for dumped tables
@@ -299,7 +410,7 @@ ALTER TABLE `known_language`
 -- AUTO_INCREMENT for table `language`
 --
 ALTER TABLE `language`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 
 --
 -- AUTO_INCREMENT for table `messages`
@@ -317,13 +428,13 @@ ALTER TABLE `premium`
 -- AUTO_INCREMENT for table `target_language`
 --
 ALTER TABLE `target_language`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
